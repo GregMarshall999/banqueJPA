88c71976549dd27a7269cf85d9cde0dcc0cf84eb
Greg <gregory.marshall999@gmail.com>
2022-05-20 14:22:05 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/dao/ClientRepository.java
@@ -0,0 +1,108 @@
+package fr.dawan.dao;
+
+import fr.dawan.entities.Client;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class ClientRepository implements IClientRepository
+{
+
+    @Override
+    public void saveClient(Client client)
+    {
+        EntityManager entityManager = IClientRepository.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try
+        {
+            transaction.begin();
+            entityManager.persist(client);
+            transaction.commit();
+        } catch (Exception e)
+        {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public void updateClient(Client client) {
+        if(client.getCode()>0)
+        {
+            EntityManager manager = IClientRepository.createEntityManager();
+            EntityTransaction transaction = manager.getTransaction();
+
+            try
+            {
+                transaction.begin();
+                manager.merge(client);
+                transaction.commit();
+            } catch (Exception e)
+            {
+                transaction.rollback();
+            } finally {
+                manager.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Client> getAllClients()
+    {
+        List<Client> clients = null;
+        EntityManager manager = IClientRepository.createEntityManager();
+
+        String sql = "SELECT entity FROM " + Client.class.getName() + " entity";
+        TypedQuery<Client> query = manager.createQuery(sql, Client.class);
+        clients = query.getResultList();
+
+        return clients;
+    }
+
+    @Override
+    public Client getClient(long code)
+    {
+        EntityManager manager = IClientRepository.createEntityManager();
+        Client client = null;
+
+        try
+        {
+            client = manager.find(Client.class, code);
+        } catch (Exception e)
+        {
+        } finally {
+            manager.close();
+        }
+
+        return client;
+    }
+
+    @Override
+    public void deleteClient(long code)
+    {
+        EntityManager manager = IClientRepository.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+
+        try
+        {
+            transaction.begin();
+            Client client = getClient(code);
+            manager.remove(client);
+            transaction.commit();
+        } catch (Exception e)
+        {
+            transaction.rollback();
+        } finally {
+            manager.close();
+        }
+    }
+
+    @Override
+    public List<Client> findCLientsByKey(String nom) {
+        return null;
+    }
+}
--- src/main/java/fr/dawan/dao/IClientRepository.java
+++ src/main/java/fr/dawan/dao/IClientRepository.java
@@ -2,6 +2,9 @@ package fr.dawan.dao;
 
 import fr.dawan.entities.Client;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public interface IClientRepository
@@ -12,4 +15,11 @@ public interface IClientRepository
     Client getClient(long code);
     void deleteClient(long code);
     List<Client> findCLientsByKey(String nom);
+
+    static EntityManager createEntityManager()
+    {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("BanqueJPA");
+        EntityManager manager = factory.createEntityManager();
+        return manager;
+    }
 }
